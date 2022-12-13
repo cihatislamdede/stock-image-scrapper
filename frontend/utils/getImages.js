d94734a6bb36d8065ab2958d337a4667374f838e
cihat dede <chtddgs@hotmail.com>
2022-12-13 17:32:52 +0300

    conver getImages to async

--- frontend/utils/getImages.js
+++ frontend/utils/getImages.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 
-export const getImages = async (term, exclude) => {
-  const baseURL = "http://127.0.0.1:8000/images/search?q=";
+const BASE_URL = "http://127.0.0.1:8000";
+const SOURCES = ["stocksnap", "adobestock", "burst", "freepik", "unsplash"];
 
-  let url = baseURL + term;
-  if (exclude) {
-    url += "&exclude=" + exclude;
-  }
-  const res = await axios.get(url);
-  return res.data;
+export const getImages = async (query, exclude) => {
+  exclude = exclude || [];
+  let data = {};
+  Promise.all(
+    SOURCES.map(async (source) => {
+      if (exclude.includes(source)) return;
+      const response = await axios.get(
+        `${BASE_URL}/search/${source}?q=${query}`
+      );
+      data[source] = response.data;
+    })
+  ).then(() => {
+    console.log("data", data);
+    return data;
+  });
 };
